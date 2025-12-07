ff5b8521512dceb8c792a6addd5574bfb669aaf8
Mckenshel Cameau <mcameau20@gmail.com>
2025-12-06 21:54:05 -0500

    updated js to switch between sections

--- mckenshel-site/src/app.js
+++ mckenshel-site/src/app.js
@@ -9,4 +9,29 @@ import Particles from 'particlesjs';
         color:'#F9F6EE'
 
     });
+
+
+const main = document.querySelector(".main");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+
+navLinks.forEach(link =>{
+    link.addEventListener('click',(e)=>{
+        e.preventDefault();
+        
+        const targetId = link.getAttribute('href').substring(1);
+
+        sections.forEach(section => section.style.display='none');
+
+        
+       
+
+        const sectionToShow = document.getElementById(targetId);
+           if (sectionToShow) {
+            sectionToShow.style.display='block';
+        }
+    })
+})
+
 }
